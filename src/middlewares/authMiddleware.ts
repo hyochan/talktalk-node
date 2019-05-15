09da6694a17f6ac478d2e3b576a9674b4ca46d96
Hyeseong Kim <cometkim.kr@gmail.com>
2019-05-15 19:18:21 +0900

    Fix formatting

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 import { ServerContext, ServerJWT } from 'types';
 
-export function createAuthMiddleware(appSecret?: string): IMiddleware<any, ServerContext> {
+export function createAuthMiddleware (appSecret?: string): IMiddleware<any, ServerContext> {
   if (!appSecret) {
     throw new Error('The secret to encrypt JWT is must be provided');
   }
